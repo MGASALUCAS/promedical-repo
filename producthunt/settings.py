69e1f20ba21a24a4f5398bfcb3b514441457b146
Computer Wizard <maulidiadam1234@gmail.com>
2022-04-10 08:29:36 -0700

    Update settings.py

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': '******',   #Hidded for security issues.
-        'PASSWORD': '*****',   #This password has hided for security isssues.
+        'PASSWORD': '*****',   #This password has hidded for security isssues.
         'HOST': 'localhost',
         'PORT': '5432',
 
