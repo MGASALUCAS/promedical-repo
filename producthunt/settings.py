e578237e4b5ef5ebdc912416a900db7ed66953f9
MGASALUCAS <mgasa.loucat1@gmail.com>
2022-08-13 13:24:42 +0300

    added  allowed host

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^nw!2cufxfb#mj7ylf3&^(ot+f8l^^l+*zdsf*r(agk30=tw9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["promedical-repo.herokuapp.com"]
 
 
 # Application definition
