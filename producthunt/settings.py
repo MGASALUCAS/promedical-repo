1608565cfbeace89206da04389069e11f8e7acf2
jack kweyunga <75433841+jackkweyunga@users.noreply.github.com>
2022-08-14 07:03:19 +0300

    Update settings.py

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dj_database_url import parse as db_url
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,10 +105,11 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 #
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        'DATABASE_URL',
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        cast=db_url
+    )
 }
 
 # Password validation
